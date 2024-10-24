a224b9cc66e934b4f464ac6ea4b8ed5f2a3624cd
sheyamvictor <sheyamjoseph@gmail.com>
2024-10-24 11:13:14 +0530

    updated

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ let result=document.getElementById("result")
 
 let button = document.getElementById("button")
 
-button.addEventListener('click',function(){
-  
-let words = text.value
-let spaceCut = words.trim( )
+
+    button.addEventListener('click',function(){
+
+    let words = text.value
+    let spaceCut = words.trim( )
+    if(spaceCut.length===0){
+        result.innerHTML="enter the text"
+    }else{
 let wordsCal = spaceCut.split(" ").length
-result.innerHTML= wordsCal + "Words"
+    result.innerHTML= wordsCal + "Words"
+    }
+    
+ 
+        
+        
+        
+        })
 
 
-})
